646265e0a27f2ab2a69ee280b0ed2676c1161dfa
alperen23214 <69838294+alperen23214@users.noreply.github.com>
2021-02-12 11:37:15 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '746386265980534844', // sunucu idsi
+  ownerRole:"749628637212442635", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[], // kayıt olmayanlara verieln rol
+  jailRolu:"795119239219511307", // cezalılara verilen rol
+  enAltYetkiliRolu:[], // ilk yetkili permi
+  activity:"𝒲𝒾𝓃𝓉𝑒𝓇༄ | Yetkili Alımları Açık ✔️", // botun durumu
+  status:"online",
+  invitelink:"", //davet linki
+  symbol:"༄", // tag
+  voicechannel:"801834191414624256" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
